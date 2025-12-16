0e196f8842042008389e4eba9c41743119b24b0e
seungjoonH <hsj6831@gmail.com>
2025-12-16 17:08:20 +0900

    init: 개발환경 설정

---
+++ be/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(3000);
+}
+
+bootstrap().catch((err) => {
+  console.error('Error starting server:', err);
+  process.exit(1);
+});
+
