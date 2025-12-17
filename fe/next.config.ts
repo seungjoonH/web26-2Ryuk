ab53c3db0b1d8303ede8f967433bf55bca6d9a07
seungjoonH <hsj6831@gmail.com>
2025-12-17 22:27:21 +0900

    fix: output export 추가

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: 'export',
   
   // 개발 환경 설정
   ...(process.env.NODE_ENV === 'development' && {
